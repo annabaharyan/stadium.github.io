624a3afab1970acb35fc68ce1c40c667b4be67b3
annabaharyan <anna.baharyan91@gmain.com>
2023-05-02 16:00:40 +0400

    add js animation

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ function openMenu() {
   navbar.classList.add("ul-opened", "scale-in-top");
 }
 menu.addEventListener("click", openMenu);
+function typeText(text, i = 0) {
+  if (i < text.length) {
+    document.querySelector(".p-text").innerHTML += text[i];
+    setTimeout(function () {
+      typeText(text, i + 1);
+    }, 10);
+  }
+}
+
+typeText("Keep track of stadiums that you have visited. Find out how many stadiums you've been to across the leagues.")
