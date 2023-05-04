48d997b0eb513f17c3789ce1d27c2d61751cd472
annabaharyan <anna.baharyan91@gmain.com>
2023-05-04 14:17:08 +0400

    add favicon,menu-toggle

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const menu = document.querySelector(".menu");
 function openMenu() {
   const menuNav = document.querySelector(".navMenu");
   const navbar = document.querySelector(".navbar");
+  if (menuNav.style.display === "flex") {
+    menuNav.style.display = "none";
+  } else {
+    menuNav.style.display = "flex";
+  }
   menuNav.classList.add("open");
-  navbar.classList.add("ul-opened", "scale-in-top");
+  navbar.classList.add("ul-opened");
 }
 menu.addEventListener("click", openMenu);
 function typeText(text, i = 0) {
